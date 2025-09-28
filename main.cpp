2b8f08934057156a0ab68a968566212b4a34e400
spoooktune <asriprasana5@gmail.com>
2025-09-27 22:49:01 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+class Color{
+    // color class with values, getters, setters
+};
+
+int main(){
+    // make many color objects and display them w/ print()
+}
